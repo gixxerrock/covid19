e10247d68c93d61fcea9451b6b35a6fd56982063
Shawn Best <sbest@blueshiftinc.com>
2020-04-14 21:26:21 -0700

    smooth function

--- code/analyze.py
+++ code/analyze.py
@@ -18,8 +18,32 @@ def AnalyzeRegion(region):
         region['deaths_daily'].append(deathDaily)
         region['deaths_daily_pm'].append(deathDaily*1000000/region['population'])
         
+    SmoothSeries(region['deaths_daily'])
+    SmoothSeries(region['deaths_daily_pm'])
+
+
+def SmoothSeries(series):
+    num = len(series)
+
+    #initialize array
+    smooth = []
+    for i in range(0, num):
+        smooth.append(0)
+
+    for i in range(1, num - 1):
+        cur = series[i]
+        smooth[i-1] += cur*0.2
+        smooth[i]   += cur*0.6
+        smooth[i+1] += cur*0.2
+
+    smooth[num-1] = series[num-1]
+    smooth[num-2] += series[num-1]*0.2
+
+    for i in range(0, num):
+        series[i] = smooth[i]
 
 def AnalyzeRegions(regionList):
     for region in regionList:
         AnalyzeRegion(region)
+    
         
